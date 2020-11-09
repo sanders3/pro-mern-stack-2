a8bb21dd598956fe654fdce2a105608a582fded7
Mark Sanders <mark.a.sanders@capgemini.com>
2020-11-09 13:27:05 +0000

    Compile App.jsx using babel in advance.
    Note that I'm checking in the compiled App.js - not best practice.

---
+++ public/App.js
@@ -0,0 +1,5 @@
+const element = /*#__PURE__*/React.createElement("div", {
+  title: "Outer div",
+  className: "banner"
+}, /*#__PURE__*/React.createElement("h1", null, "Hello World!!"));
+ReactDOM.render(element, document.getElementById('content'));
