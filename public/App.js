a70f14a43d1640fe2a80317db8313cf6ce1cc036
Mark Sanders <mark.a.sanders@capgemini.com>
2020-11-09 13:44:21 +0000

    Setting up babel plugins and presets.

--- public/App.js
+++ public/App.js
@@ -1,9 +1,11 @@
-const continents = ['Asia', 'Africa', 'America', 'Australia', 'Europe'];
-const helloContinents = Array.from(continents, function (c) {
-  return `Hello, ${c}!`;
+"use strict";
+
+var continents = ['Asia', 'Africa', 'America', 'Australia', 'Europe'];
+var helloContinents = Array.from(continents, function (c) {
+  return "Hello, ".concat(c, "!");
 });
-const message = helloContinents.join(' ');
-const element = /*#__PURE__*/React.createElement("div", {
+var message = helloContinents.join(' ');
+var element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div",
   className: "banner"
 }, /*#__PURE__*/React.createElement("h1", null, message));
