06e5744b9d6bebda3e4d6099132b13e0d9ce917b
Mark Sanders <mark.a.sanders@capgemini.com>
2020-11-09 13:34:53 +0000

    Hello, continents - showing arrow methods.

--- public/App.js
+++ public/App.js
@@ -1,5 +1,8 @@
+const continents = ['Asia', 'Africa', 'America', 'Australia', 'Europe'];
+const helloContinents = Array.from(continents, c => `Hello, ${c}!`);
+const message = helloContinents.join(' ');
 const element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div",
   className: "banner"
-}, /*#__PURE__*/React.createElement("h1", null, "Hello World!!"));
+}, /*#__PURE__*/React.createElement("h1", null, message));
 ReactDOM.render(element, document.getElementById('content'));
